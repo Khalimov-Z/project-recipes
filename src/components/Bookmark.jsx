5797501fb10e603f01a076e137dd45a997c28533
Movsar <movsar1991.mb@gmail.com>
2021-05-04 11:25:12 +0300

    laja

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,17 +1,12 @@
 import { useSelector } from 'react-redux';
-import { Link, Route } from 'react-router-dom';
-import Recipes from './Recipes/Recipes';
+import { Link } from 'react-router-dom';
 
 function Bookmark() {
-  const recipes = useSelector((state) => state.items);
+  const recipes = useSelector((state) => state.recipes.items);
   const newRecipes = recipes.filter((item) => item.favorite === true);
+
   return (
     <div className="mb-5">
-      <div>
-        <Route path="/categories/:category?/:id?/:title?">
-          <Recipes />
-        </Route>
-      </div>
       {newRecipes.map((item) => {
         return (
           <div
@@ -19,34 +14,32 @@ function Bookmark() {
             className="recipes justify-content-center m-auto"
           >
             <div className="recipes-block">
-              <Route exact path="/:category?/:id?">
-                <Link to={`/categories/all-recipes/${item.id}/${item.title}`}>
-                  <div className="d-inline-flex">
-                    <div className="img mb-3">
-                      <img width={286} height={180} src={item.url} alt="img" />
-                    </div>
-                    <div className="d-block p-3 align-self-center">
-                      <h4>{item.title}</h4>
-                      <div className="d-inline d-flex">
-                        <div>{item.calories}</div>
-                        <div
-                          style={{ borderRight: '1px solid darkgrey' }}
-                          className="ml-4 mr-4"
-                        >
-                          {' '}
-                        </div>
-                        <div>{item.time}</div>
+              <Link to={`/recipes/${item.id}`}>
+                <div className="d-inline-flex">
+                  <div className="img mb-3">
+                    <img width={286} height={180} src={item.url} alt="img" />
+                  </div>
+                  <div className="d-block p-3 align-self-center">
+                    <h4>{item.title}</h4>
+                    <div className="d-inline d-flex">
+                      <div>{item.calories}</div>
+                      <div
+                        style={{ borderRight: '1px solid darkgrey' }}
+                        className="ml-4 mr-4"
+                      >
+                        {' '}
                       </div>
+                      <div>{item.time}</div>
                     </div>
                   </div>
-                  <div
-                    style={{ borderTop: '1px solid darkgrey' }}
-                    className="pb-3"
-                  >
-                    {' '}
-                  </div>
-                </Link>
-              </Route>
+                </div>
+                <div
+                  style={{ borderTop: '1px solid darkgrey' }}
+                  className="pb-3"
+                >
+                  {' '}
+                </div>
+              </Link>
             </div>
           </div>
         );
